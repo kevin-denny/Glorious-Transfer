66ca38f9313b3b7b11c8f0eea690bd7b835563be
ShaleelSandeepa <shaleelsandeepa7@gmail.com>
2025-11-19 01:46:54 +0530

    Add pickup and destination fields to tours table and update status formatting

--- mysql-setup.sql
+++ mysql-setup.sql
@@ -5,3 +5,14 @@
 USE glorious_transfer;
 
 -- =============================================
+
+-- 19-11-2025
+
+UPDATE tours
+SET status='Pending' WHERE status = 'pending';
+
+ALTER TABLE tours
+ADD COLUMN pickup CHAR(255);
+
+ALTER TABLE tours
+ADD COLUMN destination CHAR(255);

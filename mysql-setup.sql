176b85a8939876918854cf938ea87b2d23b30407
ShaleelSandeepa <shaleelsandeepa7@gmail.com>
2025-11-22 22:46:46 +0530

    Add new columns for last login timestamp and activity log status

--- mysql-setup.sql
+++ mysql-setup.sql
@@ -16,3 +16,11 @@ ADD COLUMN pickup CHAR(255);
 
 ALTER TABLE tours
 ADD COLUMN destination CHAR(255);
+
+-- 22-11-2025
+
+ALTER TABLE auth_users
+ADD COLUMN last_login_at TIMESTAMP;
+
+ALTER TABLE activity_logs
+ADD COLUMN status VARCHAR(50);
